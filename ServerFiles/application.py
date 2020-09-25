3a1abeeda38e46b94d832376d107ac3bb789c4bc
EC2 Default User <ec2-user@ip-172-30-5-128.ec2.internal>
2020-09-25 01:53:58 +0000

    Added some template examples in placeholder.html

--- ServerFiles/application.py
+++ ServerFiles/application.py
@@ -3,6 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def placeholder():
-    return render_template('placeholder.html')
+    my_variable = "This is a test string that will be passed to the HTML template"
+    my_list = ["This", "is", "a", "list", "of", "strings"]
+    
+    #Example of using a template to render HTML
+    #"Test" is a variable that we declare that will be passed to the HTML template. We can call that whatever we want.
+    return render_template('placeholder.html', test = my_variable, a_number = 3, loopable_list = my_list)
     
 app.run(host='0.0.0.0', port=8080)
